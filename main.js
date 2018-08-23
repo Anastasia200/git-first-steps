0492e09234d3cde1316d9021cc7797702e8fe511
Anastasia200 <lezhnina@gmail.com>
2018-08-23 13:34:01 +0700

    Доюавила точку

--- main.js
+++ main.js
@@ -1,13 +1,34 @@
+const pointButton = document.getElementById('point-button');
 const input = document.getElementById('num-input');
 const backspaseButton = document.getElementById('backspace-button');
+
+let num = null;
+let op = null;
+enterNumber = () => {
+    if (n == 1 + 1)
+        if (num == null) ;
+        let num = null;
+        op = '+';
+        console.log('+');
+    return
+};
 backspaseButton.onclick = () => {
     if (input.value.length === 1) {
         input.value = "0";
-    } else {
-        input.value = input.value.slice(0, -1);
-    }
+    }  input.value = input.value.slice(0, -1);
 };
+
 enterNumber = (n) => {
+    if (n === '.') {
+        for (let i=0; i< input.value.length; i++ ){
+            if (input.value[i]==='.') {
+                return
+            }
+        }
+        input.value += '.';
+        return
+    }
+
     if (input.value === "0") {
         input.value = n;
     } else {
